1dff93657eb3e5bf4026a381d0cd2e049877bd6b
ezziechokah <chokahezra7786@gmail.com>
2025-03-19 11:59:14 +0300

    Add files via upload

---
+++ inua.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Example form validation (for simplicity)
+document.querySelectorAll("form").forEach(form => {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      alert("Form submitted successfully! Thank you for engaging with Inua Mwanaume Foundation.");
+    });
+  });
+  
